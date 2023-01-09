83cb063630fd266ba9ffd2c2b88bcbc08a61a181
Allassane <dickoallassane1997@gmail.com>
2023-01-09 14:59:24 +0100

    Add and render Calculator module

---
+++ src/modules/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Calcuclator extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <h1>Hello title</h1>
+    )
+  }
+}
